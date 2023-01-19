672d074c33cffc55c125f28d4affd448b227d4e7
cj0673 <51242946+cj0673@users.noreply.github.com>
2023-01-19 12:14:54 +0800

    Modify headers

--- soup_weather_day.py
+++ soup_weather_day.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent': 'Chrome/66.0.3359.181'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
 url = 'https://weather.com/zh-TW/weather/today/l/25.0076143,121.5053019'
 web = requests.get(url, headers=headers)
-
 soup = BeautifulSoup(web.text, 'html.parser')
+
 weather = soup.select('span.CurrentConditions--timestamp--1ybTk')
 for a in weather:
     b = a.text
